5e63fca9329af38562e9fbdcc30edfaf57494764
madarasz <mr.madarasz.istvan@gmail.com>
2018-12-24 21:49:40 +0100

    random-mars: use index mappings for arrays

--- resources/views/script/random-mars.blade.php
+++ resources/views/script/random-mars.blade.php
@@ -3,9 +3,10 @@
         el: '#random-mars',
         data: {
             playerNumber: 2,
-            corporations: [],
+            showResults: false,
+            // arrays to randomize from
+            corporations: [], // from API
             boards: ['base game', 'Hellas', 'Elysium'],
-            corporations: [],
             colonies: ['Luna', 'Ceres', 'Triton', 'Ganymede', 'Calisto', 'Io', 'Europa', 'Enceladus', 'Miranda', 'Titan', 'Pluto'],
             milestones: [
                 'terraformer', 'mayor', 'gardener', 'builder', 'planner', 
@@ -19,17 +20,35 @@
                 'cultivator', 'magnate', 'space baron', 'excentric', 'contractor',
                 'venus'
             ],
-            chosenBoard: "",
-            chosenCorporations: [],
-            chosenColonies: [],
-            chosenMilestones: [],
-            chosenAwards: [],
-            showResults: false
+            // which indexes are selected
+            boardMapping: 0,
+            corporationMapping: [],
+            milestoneMapping: [],
+            awardMapping: [],
+            colonyMapping: []
         },
         mounted: function() {
+            // get Mars Corporations from API
             axios.get('/api/games/7').then(function (response) {
                 randomMars.corporations = response.data.factions.map(obj => ({name: obj.name, icon: obj.iconFile}));
             });
+        },
+        computed: {
+            chosenBoard: function() {
+                return this.boards[this.boardMapping];
+            },
+            chosenCorporations: function() {
+                return this.corporationMapping.map(i => this.corporations[i]);
+            },
+            chosenColonies: function() {
+                return this.colonyMapping.map(i => this.colonies[i]);
+            },
+            chosenMilestones: function() {
+                return this.milestoneMapping.map(i => this.milestones[i]);
+            },
+            chosenAwards: function() {
+                return this.awardMapping.map(i => this.awards[i]);
+            }
         },
         methods: {
             filename: function(name) {
@@ -42,43 +61,52 @@
                 this.showResults = true;
             },
             randomBoard: function() {
-                this.chosenBoard = this.boards[Math.floor(Math.random() * this.boards.length)];
+                this.boardMapping = Math.floor(Math.random() * this.boards.length);
                 this.randomMilestones();
                 this.randomAwards();
             },
+            // returns array of X elements from [0, 1, ..., n-1] array
+            randomElements: function(x, n) {
+                var result = [];
+                while (result.length < x) {
+                    var element = Math.floor(Math.random()*n);
+                    if(result.indexOf(element) < 0) result.push(element);
+                }
+                return result;
+            },
+            // returns a random element which is not present in mapping array
+            randomOneElement: function(mapping, max) {
+                var n;
+                do {
+                    n = Math.floor(Math.random()*max);
+                } while (mapping.indexOf(n) > -1);
+                return n;
+            },
             randomCorps: function() {
-                // magic
-                this.chosenCorporations = this.corporations.map(x => ({ x, r: Math.random() })).sort((a,b) => a.r - b.r).map(a => a.x).slice(0, this.playerNumber+2);
+                this.corporationMapping = this.randomElements(this.playerNumber+2, this.corporations.length);
             },
             randomCorp: function(index) {
-                var difference = this.corporations.filter(x => !this.chosenCorporations.includes(x));
-                Vue.set(this.chosenCorporations, index, difference[Math.floor(Math.random() * difference.length)]);
+                Vue.set(this.corporationMapping, index, this.randomOneElement(this.corporationMapping, this.corporations.length));
             },
             randomColonies: function() {
-                // magic
-                this.chosenColonies = this.colonies.map(x => ({ x, r: Math.random() })).sort((a,b) => a.r - b.r).map(a => a.x).slice(0, this.playerNumber+2);
+                this.colonyMapping = this.randomElements(this.playerNumber+2, this.colonies.length);
             },
             randomColony: function(index) {
-                var difference = this.colonies.filter(x => !this.chosenColonies.includes(x));
-                Vue.set(this.chosenColonies, index, difference[Math.floor(Math.random() * difference.length)]);
+                Vue.set(this.colonyMapping, index, this.randomOneElement(this.colonyMapping, this.colonies.length));
             },
             randomMilestones: function() {
                 var len = this.chosenBoard == 'Hellas' ? 5 : 6;
-                // magic
-                this.chosenMilestones = this.milestones.map(x => ({ x, r: Math.random() })).sort((a,b) => a.r - b.r).map(a => a.x).slice(0, len);
+                this.milestoneMapping = this.randomElements(len, this.milestones.length);
             },
             randomMilestone: function(index) {
-                var difference = this.milestones.filter(x => !this.chosenMilestones.includes(x));
-                Vue.set(this.chosenMilestones, index, difference[Math.floor(Math.random() * difference.length)]);
+                Vue.set(this.milestoneMapping, index, this.randomOneElement(this.milestoneMapping, this.milestones.length));
             },
             randomAwards: function() {
                 var len = this.chosenBoard == 'Elysium' ? 5 : 6;
-                // magic
-                this.chosenAwards = this.awards.map(x => ({ x, r: Math.random() })).sort((a,b) => a.r - b.r).map(a => a.x).slice(0, len);
+                this.awardMapping = this.randomElements(len, this.awards.length);
             },
             randomAward: function(index) {
-                var difference = this.awards.filter(x => !this.chosenAwards.includes(x));
-                Vue.set(this.chosenAwards, index, difference[Math.floor(Math.random() * difference.length)]);
+                Vue.set(this.awardMapping, index, this.randomOneElement(this.awardMapping, this.awards.length))
             },
         }
     })
