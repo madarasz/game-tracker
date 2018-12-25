8340800a80e2fcab5b1fe812e8e99c19746b0916
madarasz <mr.madarasz.istvan@gmail.com>
2018-12-25 08:06:42 +0100

    disable google translate

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 <head>
     <title>Gametracker</title>
     {{--CSRF token--}}
@@ -8,7 +8,7 @@
     </script>
     {{--compiled css--}}
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-
+    <meta name="google" content="notranslate">
 </head>
 <body>
     <div class="own-container">
