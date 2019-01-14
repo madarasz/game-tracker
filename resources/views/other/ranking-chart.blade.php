2192ad71ef1c92b599a874dc9f961c4937763fce
madarasz <mr.madarasz.istvan@gmail.com>
2019-01-14 12:03:23 +0100

    ELO chart hidden if there are no concluded sessions

--- resources/views/other/ranking-chart.blade.php
+++ resources/views/other/ranking-chart.blade.php
@@ -4,8 +4,8 @@
             ELO history<br/>
             @include('other.season-string')
         </h5>
-        <div id="chart-elo-history"></div>
-        <div class="small-text" v-if="game.sessionCount == 0">
+        <div id="chart-elo-history" v-if="sessionList.length > 0"></div>
+        <div class="small-text" v-if="sessionList.length == 0">
             no concluded sessions yet
         </div>
     </div>
