ed3a92df21042944b51622900db1c0f08b37b034
madarasz <mr.madarasz.istvan@gmail.com>
2019-01-16 14:10:15 +0100

    elo chart bug fix

--- resources/views/other/ranking-chart.blade.php
+++ resources/views/other/ranking-chart.blade.php
@@ -4,7 +4,7 @@
             ELO history<br/>
             @include('other.season-string')
         </h5>
-        <div id="chart-elo-history" v-if="sessionList.length > 0"></div>
+        <div id="chart-elo-history"></div>
         <div class="small-text" v-if="sessionList.length == 0">
             no concluded sessions yet
         </div>
